390fdebbf294e53ffb138da6fe56b1865ab6626f
Ashansa Mahra <mahraashansa1997@gmail.com>
2024-01-03 19:05:16 +0530

    Add files via upload

---
+++ Sum_of_Two_Integers/Program.cs
@@ -0,0 +1,19 @@
+﻿class Solution
+{
+    static int SolveMeFirst(int a, int b)
+    {
+        if (a >= 1 && b <= 1000)
+        {
+            return (a + b);
+        }
+        return 0;
+    }
+
+    static void Main(String[] args)
+    {
+        int val1 = Convert.ToInt32(Console.ReadLine());
+        int val2 = Convert.ToInt32(Console.ReadLine());
+        int sum = SolveMeFirst(val1, val2);
+        Console.WriteLine(sum);
+    }
+}
